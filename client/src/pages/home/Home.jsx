473309aef4d0277b3e859b9b4dbc3bbfe35b6abf
Hillarysidney <hillarysidney4@gmail.com>
2024-02-29 17:37:12 +0000

    Home page

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import Header from '../../components/header/Header';
+import Posts from '../../components/posts/Posts';
+import Sidebar from '../../components/sidebar/Sidebar';
+import './home.scss';
+import axios from "axios";
+import { useLocation } from 'react-router';
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const {search} = useLocation();
+
+  useEffect(() => {
+   const fetchPosts = async ()=>{
+    try {
+      const res = await axios.get("/posts" + search);
+      console.log(res);
+      setPosts(res.data);
+    } catch (err) {
+      console.log(err)
+    };
+   };
+   fetchPosts();
+  }, [search]);
+  
+  return (
+    <>
+      <Header />
+      <div className='home'>
+        <Posts posts={posts}/>
+        <Sidebar />
+      </div>
+    </>
+  )
+}
